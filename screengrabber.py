f0e77caa68c24b6cee60b39418e988e7d08263f7
hongminy <39747015+hongminy@users.noreply.github.com>
2019-03-04 23:19:02 -0800

    add stuff followed p.5

--- screengrabber.py
+++ screengrabber.py
@@ -1,12 +1,20 @@
-import numpy as np
-from PIL import ImageGrab
-import cv2
-import time
-from directKey import PressKey, ReleaseKey, W, A, S, D
-from Debuger import Debuger
+import     numpy     as      np
+from       PIL       import  ImageGrab
+import     cv2
+import     time
+from       directKey import  PressKey, ReleaseKey, W, A, S, D
+from       Debuger   import Debuger
+import     pyautogui
 
 debug = False
 
+def get_region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
+    cv2.fillPoly(mask, vertices, 255)
+    # filled that mask by the vertices
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+    
 
 
 def waitsecs(sec = 5):
@@ -17,10 +25,20 @@ def waitsecs(sec = 5):
     print("READY!GO")
 
 def process_image(originalImage):
+    # convert the given image to grey
     processedImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
     processedImage = cv2.Canny(processedImage,
                                threshold1 = 200,
                                threshold2 = 300)
+    
+    vertices = np.array([[10,500], [10,300], [300,200],
+                        [500,200], [800,300], [800,500]])
+    # this array is twickable, and this one is good for scooter
+    # different vehicles need differents param here
+    # cutting out the area of instrument panel (1st person view)or 
+    # the aera of vehicle itself (if it is in 3rd person view)
+    
+    processedImage = get_region_of_interest(processedImage, [vertices])
     return processedImage
 
 def get_screen():
@@ -30,11 +48,12 @@ def get_screen():
     while(True):
         if debug: debuger.deb(("Loop takes {} secs".format(time.time() - lastTime)))
         lastTime = time.time()
-        #frame = np.array(ImageGrab.grab(bbox=(130,64,880,624)))
         frame = np.array(ImageGrab.grab(bbox=(0,40,800,636)))
         edgeDetection = process_image(frame)
-        #cv2.imshow('GameCapture', cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         cv2.imshow('edge_detection', edgeDetection)
+        # display the opencv detected edges
+
+        # press q to quit
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
