79d74b88a9b4a2c111963aa72449ca2c83609597
hongminy <39747015+hongminy@users.noreply.github.com>
2019-03-04 22:28:40 -0800

    add functions follows p.3

--- screengrabber.py
+++ screengrabber.py
@@ -1,6 +1,16 @@
 import numpy as np
 from PIL import ImageGrab
 import cv2
+import time
+from directKey import PressKey, ReleaseKey, W, A, S, D
+
+
+def waitsecs(sec = 5):
+    # print 5, 4, 3, 2, 1 for users to ready
+    for i in list(range(1, sec + 1))[::-1]:
+        print(i ,'..')
+        time.sleep(1)
+    print("READY!GO")
 
 def process_image(originalImage):
     processedImage = cv2.cvtColor(originalImage, cv2.COLOR_BGR2GRAY)
@@ -10,6 +20,7 @@ def process_image(originalImage):
     return processedImage
 
 def get_screen():
+    waitsecs()
     while(True):
         #frame = np.array(ImageGrab.grab(bbox=(130,64,880,624)))
         frame = np.array(ImageGrab.grab(bbox=(0,40,800,636)))
